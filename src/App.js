72daf07cbec9f528850a19765c2107b36798760e
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-12-08 04:09:09 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {id: '1', name: 'Max', age: 28},
+      {id: '2', name: 'Jaimes', age: 22},
+      {id: '3', name: 'Arturo', age: 23}
+    ],
+    showPersons: true,
+  }
+
+  nameChangedHandler = (event, id)=>{
+    const indexOfPersonToChange = this.state.persons.findIndex(p =>{
+      return p.id === id
+    })
+    
+    const personToChange = {
+      ...this.state.persons[indexOfPersonToChange]
+    }
+
+    personToChange.name = event.target.value
+
+    const newArrayOfPersons = [...this.state.persons]
+    newArrayOfPersons[indexOfPersonToChange] = personToChange
+
+    this.setState({persons: newArrayOfPersons})
+  }
+
+  hideTheNames = ()=>{
+    this.state.showPersons === true ? this.setState({showPersons: false}) : this.setState({showPersons: true})
+  }
+
+  deletePersonHandler = (personId)=>{
+    // Delete using index
+    // const persons = [...this.state.persons]
+    // persons.splice(personIndex, 1)
+    // this.setState({
+    //   persons: persons
+    // })
+
+    //Delete using filter and id
+    const copyOfPersons = this.state.persons.filter((person)=>{
+      return person.id !== personId
+    })
+
+    this.setState({
+      persons: copyOfPersons
+    })
+
+  }
+
+  render() {
+
+    let peopleContainer;
+     if(this.state.showPersons){
+      peopleContainer = (      
+       <div>
+         {this.state.persons.map((person, index) => {
+          return <Person
+            name={person.name}
+            age={person.age}
+            click={()=>this.deletePersonHandler(person.id)}
+            key={person.id}
+            changed={(event)=>this.nameChangedHandler(event, person.id)}
+          />
+         })}
+      </div>)
+     } else {
+       peopleContainer = null;
+     }
+
+    return (
+      <div className="App">
+        <h1>Hi, I am a React App</h1>
+        <p>This is really working</p>
+        <button onClick={this.hideTheNames}>Hide the cards</button>
+        {peopleContainer}
+      </div>
+    );
+    // return React.createElement('div', null, React.createElement('h1', {className: "App"}, 'Hi, I am actually a React App'))
+  }
+}
+
+export default App;
